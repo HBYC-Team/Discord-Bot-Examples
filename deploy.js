ac99cc7fc672e98c64364415211714bdeab04e19
dragonyc1002 <mivck1012@gmail.com>
2022-07-18 20:28:46 +0800

    update

---
+++ deploy.js
@@ -0,0 +1,33 @@
+const fs = require("node:fs");
+const path = require("node:path");
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { clientId } = require("./config.json");
+
+require("dotenv").config();
+const token = process.env.TOKEN;
+
+const commands = [];
+const commandFiles = fs.readdirSync("./cmds").filter(file => file.endsWith(".js"));
+
+for(const file of commandFiles) {
+	const command = require(`./cmds/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+(async () => {
+	try {
+		console.log("Started refreshing application commands.");
+
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+
+		);
+	} catch(error) {
+		console.error(error);
+	}
+})();
