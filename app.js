3bd2aa793784d70863d382b5f75b9db771874e82
dragonyc1002 <mivck1012@gmail.com>
2022-07-22 11:42:06 +0800

    v0.1.0

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ for (const file of commandFiles) {
 const eventsPath = path.join(__dirname, "events");
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
+
 for(const file of eventFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
