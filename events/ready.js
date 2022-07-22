0c25a2b9ca7631646d88465627c87f512c39e50e
dragonyc1002 <mivck1012@gmail.com>
2022-07-22 11:41:15 +0800

    Refactored: Ready event

--- events/ready.js
+++ events/ready.js
@@ -4,12 +4,23 @@ module.exports = {
 	name: "ready",
 	once: true,
 	execute(client) {
+		let datetime = new Date().getFullYear() + "-" 
+        	 	+ (new Date().getMonth()+1) + "-" 
+         		+ new Date().getDate() + " " 
+        		+ new Date().getHours() + ":"  
+         		+ new Date().getMinutes() + ":" 
+        		+ new Date().getSeconds();
+
 		createMusicManager(client);
-		console.log(`Logged in as ${client.user.tag}`);
+		console.log("Bot Logined");
+        console.log(client.user.tag);
+        console.log("Logined at", datetime);
+        console.log("------------------------");
+
 		client.user.setPresence({ activities: [{ name: "新機上機測試中" }] });
 		console.log(client.guilds.cache.map(guild => guild.name));
 		console.log(client.guilds.cache.map(guild => guild.id));
-		console.log("---------------");
+		console.log("------------------------");
 
 	}
 }
