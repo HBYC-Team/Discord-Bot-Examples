9d72f74a5db4ed703feadb056a68b9bc4b36e3cc
dragonyc1002 <mivck1012@gmail.com>
2022-08-01 10:05:43 +0800

    Feature: Added custom strings

--- cmds/2048.js
+++ cmds/2048.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { DjsTofe } = require('@hizollo/games');
+const { tofe } = require("./strings.json");
 
-const tofe = new SlashCommandBuilder()
+const tofeData = new SlashCommandBuilder()
 	.setName("2048")
 	.setDescription("開啟一場2048遊戲")
 	.addStringOption(option => 
@@ -16,7 +17,8 @@ const tofe = new SlashCommandBuilder()
 
 
 module.exports = {
-	data: tofe,
+	data: tofeData,
+	
 	async execute(interaction) {
 		let datetime = new Date().getFullYear() + "-" 
         	 	+ (new Date().getMonth()+1) + "-" 
@@ -25,36 +27,31 @@ module.exports = {
          		+ new Date().getMinutes() + ":" 
         		+ new Date().getSeconds();
         		
-		const mode = interaction.options.getString("難度");
-		
-		if(mode === "簡單") {
-			const user = interaction.user;
-			const game = new DjsTofe({
-  				source: interaction, 
-  				players: [user]
-			});
+		const hardMode = (() => {
+			if(interaction.options.getString("難度") === "簡單模式"){
+				return false;
+			} else {
+				return true;
+			}
+		})();
 
-			await game.initialize();
-			await game.start();
-			await game.conclude();
-		} else {
-			const user = interaction.user;
-			const game = new DjsTofe({
-				hardMode: true,
-  				source: interaction, 
-  				players: [user] 
-			});
+		const game = new DjsTofe({
+			hardMode: hardMode,
+  			source: interaction, 
+  			players: [interaction.user],
+  			strings: tofe
+		});
 
-			await game.initialize();
-			await game.start();
-			await game.conclude();
-		}
+		await game.initialize();
+		await game.start();
+		await game.conclude();
 
-		console.log(`>2048 ${難度}`);
+
+		console.log(`>2048 hardMode: ${hardMode}`);
 		console.log(`from ${interaction.guild.name}`);
 		console.log(`by ${interaction.user.tag}`);
 		console.log(`at ${datetime}`);
 		console.log("------------");
 
-	},
+	}
 }
