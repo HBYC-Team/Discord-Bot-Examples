6ea64e4781880d3e640531d17a9677cc43f37616
dragonyc1002 <mivck1012@gmail.com>
2022-08-01 10:10:48 +0800

    bug fixes

--- cmds/gomoku.js
+++ cmds/gomoku.js
@@ -1,9 +1,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { DjsGomoku } = require("@hizollo/games");
+const { gomoku } = require("./strings.json");
 
-const gomoku = new SlashCommandBuilder()
+const gomokuData = new SlashCommandBuilder()
 	.setName("gomoku")
-	.setDescription("開啟一場五子棋遊戲")
+	.setDescription("開啟一場九路五子棋遊戲")
 	.addUserOption(option => 
 		option.setName("p2")
 			.setDescription("玩家2")
@@ -15,46 +16,53 @@ const gomoku = new SlashCommandBuilder()
 
 
 module.exports = {
-	data: gomoku,
-	async execute(interaction) {
+	data: gomokuData,
+
+	async execute(interaction){
 		let datetime = new Date().getFullYear() + "-" 
         	 	+ (new Date().getMonth()+1) + "-" 
          		+ new Date().getDate() + " " 
         		+ new Date().getHours() + ":"  
          		+ new Date().getMinutes() + ":" 
         		+ new Date().getSeconds();
-		
-		const { username, id, tag } = interaction.options.getUser("p2");
-		const user3 = interaction.options.getUser("p3");
-		const name3 = user3.username;
-		const id3 = user3.id;
-		const game = new DjsGomoku({
-  			source: interaction, 
-  			players: [{
-    			username: interaction.user.username, 
-    			id: interaction.user.id, 
-    			symbol: '🔵'
- 			}, {
-    			username: username, 
-    			id: id, 
-    			symbol: '🔴'
-    		}, {
-    			username: name3,
-    			id: id3,
-    			symbol: '🟢'
-  			}] 
-		});
 
-		await game.initialize();
-		await game.start();
-		await game.conclude();
 
-		console.log(`>2048 ${tag} ${user3.tag}`);
+        const p2 = interaction.options.getUser("p2");
+        const p3 = interaction.options.getUser("p3");
+
+        const players = [{
+        	username: interaction.user.username,
+        	id: interaction.user.id,
+        	symbol: "🔵"
+        }, {
+        	username: p2.username,
+        	id: p2.id,
+        	symbol: "🔴"
+        }];
+
+        if(p3 !== null){
+        	players.push({
+        		username: p3.name,
+        		id: p3.id,
+        		symbol: "🟢" 
+        	});
+        }
+
+        const game = new DjsGomoku({
+        	source: interaction,
+        	players: players,
+        	strings: gomoku
+        });
+
+        await game.initialize();
+        await game.start();
+        await game.conclude();
+
+        console.log(`>gomoku`);
 		console.log(`from ${interaction.guild.name}`);
 		console.log(`by ${interaction.user.tag}`);
 		console.log(`at ${datetime}`);
 		console.log("------------");
-
-	},
+	}
 }
 
