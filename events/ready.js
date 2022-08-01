8680aaf93acc04d5ebc95616ecd35e7cf5e511fb
dragonyc1002 <mivck1012@gmail.com>
2022-08-01 10:18:24 +0800

    Changed: status

--- events/ready.js
+++ events/ready.js
@@ -11,13 +11,17 @@ module.exports = {
          		+ new Date().getMinutes() + ":" 
         		+ new Date().getSeconds();
 
-		createMusicManager(client);
+  		createMusicManager(client, {
+    		defaultMaxQueueSize: Infinity,
+    		enableInlineVolume: true
+		});
+		
 		console.log("Bot Logined");
         console.log(client.user.tag);
         console.log("Logined at", datetime);
         console.log("------------------------");
 
-		client.user.setPresence({ activities: [{ name: "新機上機測試中" }] });
+		client.user.setPresence({ activities: [{ name: "v0.2.0已經發布！" }] });
 		console.log(client.guilds.cache.map(guild => guild.name));
 		console.log(client.guilds.cache.map(guild => guild.id));
 		console.log("------------------------");
