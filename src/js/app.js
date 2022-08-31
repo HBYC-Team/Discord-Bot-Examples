1dc8b246bb80182c7653c57593a891f404ec5475
dragonyc1002 <mivck1012@gmail.com>
2022-08-31 13:24:50 +0800

    chore: fixed some function

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,27 @@
 /*****************************************
  ************HBYC Discord Bot*************
  **********Author: dragonyc1002***********
- **********License: Apache 2.0**********
- *************Version: 1.0.0*************
+ ***********License: CC-BY-4.0************
+ *************Version: 1.0.0**************
  ********Release Date: 2022-08-15*********
  *****************************************/
 const { Client, Collection, GatewayIntentBits, Partials, InteractionType } = require("discord.js");
 const { createMusicManager } = require("@kyometori/djsmusic");
 const { lanBot, dalao, mention, lanDino, www } = require("../config.json");
+const { banList } = require("../banList.json");
 const fs = require("fs");
 const path = require("path");
 
 require("dotenv").config();
 const token = process.env.TOKEN;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates,GatewayIntentBits.GuildPresences,GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent], partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] });
-
-let date = new Date();
-let datetime = date.getFullYear() + "-"
-			+ (date.getMonth() + 1) + "-"
-            + date.getDate() + " "
-			+ date.getHours() + ":"
-            + date.getMinutes() + ":"
-            + date.getSeconds();
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildVoiceStates,GatewayIntentBits.GuildPresences,GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent], partials: [Partials.Channel, Partials.Message, Partials.User, Partials.GuildMember, Partials.Reaction] });
 	
 client.on("interactionCreate", async interaction => {
 
 	if(!interaction.type === InteractionType.ApplicationCommand) return;
 	
-	//if(interaction.user.id === banList)
+	if(banList.includes(interaction.user.id)) return;
 
 	const command = client.commands.get(interaction.commandName);
 
@@ -37,8 +30,14 @@ client.on("interactionCreate", async interaction => {
 	try {
 		await command.execute(interaction);
 	} catch(error){
-	 	await interaction.reply("指令出現問題，已經將錯誤訊息私訊了喔！");
 		await interaction.user.send(`錯誤訊息：${error.message}\n有問題歡迎使用/report指令喔！`);
+		let date = new Date();
+		let datetime = date.getFullYear() + "-"
+				+ (date.getMonth() + 1) + "-"
+            	+ date.getDate() + " "
+				+ date.getHours() + ":"
+	            + date.getMinutes() + ":"
+	            + date.getSeconds();
 		console.log(`Error at:${datetime}\n ${error}`);
 		console.log("--");
 	}
@@ -73,58 +72,167 @@ for(const file of eventFiles) {
 
 client.on('messageCreate', async message => {
 	if(message.author.bot) return;
+	if(banList.includes(message.author.id)) return;
+
+	if(message.channel.type == "DM") {
+		console.log("有人私訊機器人");
+		return;
+	};
 
 	if(message.content === "爛bot" || message.content === "lan bot" || message.content === "爛Bot" || message.content === "爛BOT"){
 		let replies = Math.floor(Math.random()*lanBot.length);
 		let replyMsg = lanBot[replies];
-		await message.channel.send(replyMsg);
-		console.log("爛bot");
-		console.log(`from ${interaction.guild.name}`);
-		console.log(`by ${interaction.user.username}`);
-		console.log(`At ${datetime}`);
-		console.log("------");
+		let date = new Date();
+		let datetime = date.getFullYear() + "-"
+				+ (date.getMonth() + 1) + "-"
+	            + date.getDate() + " "
+				+ date.getHours() + ":"
+	            + date.getMinutes() + ":"
+	            + date.getSeconds();
+		try{
+			await message.channel.send(replyMsg);
+			console.log("爛bot");
+			console.log(`from ${message.guild.name}`);
+			console.log(`by ${message.author.tag}`);
+			console.log(`At ${datetime}`);
+			console.log("------");
+		} catch(error){
+			const channelName = client.channels.cache.get(message.channelId);
+			await message.author.send(`我在${message.guild.name}的${channelName}沒有發送訊息的權限喔，請給我發送訊息的權限後再試一次`);
+			console.log(`Error at:${datetime}\n ${error}`);
+			console.log("--")
+		}
 	};
 
 	if(message.content === "x04bot"){
-		await message.channel.send("要罵人之前請先記得切換輸入法呦;)");
-		console.log("x04bot");
-		console.log(`from ${interaction.guild.name}`);
-        console.log(`by ${interaction.user.username}`);
-        console.log(`At ${datetime}`);
-		console.log("------");
+		let date = new Date();
+		let datetime = date.getFullYear() + "-"
+				+ (date.getMonth() + 1) + "-"
+	            + date.getDate() + " "
+				+ date.getHours() + ":"
+	            + date.getMinutes() + ":"
+	            + date.getSeconds();
+		try{
+			await message.channel.send("要罵人之前請先記得切換輸入法呦;)");
+			console.log("x04bot");
+			console.log(`from ${message.guild.name}`);
+	        console.log(`by ${message.author.tag}`);
+	        console.log(`At ${datetime}`);
+			console.log("------");
+		} catch(error){
+			const channelName = client.channels.cache.get(message.channelId);
+
+			if(message.guild === null){
+				console.log("Type: Direct Message")
+				console.log(`by ${message.author.tag}`);
+	        	console.log(`At ${datetime}`);
+				console.log("------");
+				return;
+			} else {
+				await message.author.send(`我在${message.guild.name}的${channelName}沒有發送訊息的權限喔，請給我發送訊息的權限後再試一次`);
+			}
+			console.log(`Error at:${datetime}\n ${error}`);
+			console.log("--")
+		}
 	};
 
 	if(message.content === "大佬" || message.content === "lao" || message.content === "佬"){
 		let replies = Math.floor(Math.random()*dalao.length);
 		let replyMsg = dalao[replies];
-		await message.channel.send(replyMsg);
-		console.log("dalao");
-		console.log(`from ${interaction.guild.name}`);
-        console.log(`by ${interaction.user.username}`);
-        console.log(`At ${datetime}`);
-		console.log("------");
+		let date = new Date();
+		let datetime = date.getFullYear() + "-"
+				+ (date.getMonth() + 1) + "-"
+	            + date.getDate() + " "
+				+ date.getHours() + ":"
+	            + date.getMinutes() + ":"
+	            + date.getSeconds();
+		try {
+			await message.channel.send(replyMsg);
+			console.log("dalao");
+			console.log(`from ${message.guild.name}`);
+	        console.log(`by ${message.author.tag}`);
+	        console.log(`At ${datetime}`);
+			console.log("------");
+		} catch(error){
+			const channelName = client.channels.cache.get(message.channelId);
+
+			if(message.guild === null){
+				console.log("Type: Direct Message")
+				console.log(`by ${message.author.tag}`);
+	        	console.log(`At ${datetime}`);
+				console.log("------");
+				return;
+			} else {
+				await message.author.send(`我在${message.guild.name}的${channelName}沒有發送訊息的權限喔，請給我發送訊息的權限後再試一次`);
+			}
+			console.log(`Error at:${datetime}\n ${error}`);
+			console.log("--")		
+		}
 	};
 
 	if(message.mentions.has(client.user.id)){
 		let replies = Math.floor(Math.random()*mention.length);
 		let replyMsg = mention[replies];
-		await message.channel.send(replyMsg);
-		console.log("Got tag");
-		console.log(`from ${interaction.guild.name}`);
-        console.log(`by ${interaction.user.username}`);
-        console.log(`At ${datetime}`);
-		console.log("------");
+		let date = new Date();
+		let datetime = date.getFullYear() + "-"
+				+ (date.getMonth() + 1) + "-"
+	            + date.getDate() + " "
+				+ date.getHours() + ":"
+	            + date.getMinutes() + ":"
+	            + date.getSeconds();
+		try {
+			await message.channel.send(replyMsg);
+			console.log("Got tag");
+			console.log(`from ${message.guild.name}`);
+	        console.log(`by ${message.author.tag}`);
+	        console.log(`At ${datetime}`);
+			console.log("------");
+		} catch(error){
+			const channelName = client.channels.cache.get(message.channelId);
+
+			if(message.guild === null){
+				console.log("Type: Direct Message")
+				console.log(`by ${message.author.tag}`);
+	        	console.log(`At ${datetime}`);
+				console.log("------");
+				return;
+			} else {
+				await message.author.send(`我在${message.guild.name}的${channelName}沒有發送訊息的權限喔，請給我發送訊息的權限後再試一次`);
+			}
+			console.log(`Error at:${datetime}\n ${error}`);
+			console.log("--")
+		}
 	};
 
 	if(message.content === "爛恐龍" || message.content === "恐龍很爛"){
 		let replies = Math.floor(Math.random()*lanDino.length);
 		let replyMsg = lanDino[replies];
-		await message.channel.send(replyMsg);
-		console.log("爛恐龍");
-		console.log(`from ${interaction.guild.name}`);
- 	    console.log(`by ${interaction.user.username}`);
-        console.log(`At ${datetime}`);
-		console.log("------");
+		let date = new Date();
+		let datetime = date.getFullYear() + "-"
+				+ (date.getMonth() + 1) + "-"
+	            + date.getDate() + " "
+				+ date.getHours() + ":"
+	            + date.getMinutes() + ":"
+	            + date.getSeconds();
+		try {
+			await message.channel.send(replyMsg);
+			console.log("爛恐龍");
+			console.log(`from ${message.guild.name}`);
+ 	    	console.log(`by ${message.author.tag}`);
+        	console.log(`At ${datetime}`);
+			console.log("------");
+		} catch(error){
+			const channelName = client.channels.cache.get(message.channelId);
+			if(message.guild === null){
+				console.log("Type: Direct Message")
+				console.log(`by ${message.author.tag}`);
+	        	console.log(`At ${datetime}`);
+				console.log("------");
+				return;
+			} else {
+				await message.author.send(`我在${message.guild.name}的${channelName}沒有發送訊息的權限喔，請給我發送訊息的權限後再試一次`);
+			}
+		}
 	};
 
 	if(message.content === "w" || message.content === "ww" || message.content === "www" || message.content === "wwww" || message.content === "wwwww" || message.content === "wwwwww"){
@@ -134,12 +242,36 @@ client.on('messageCreate', async message => {
 		if(replyTrueFalse == "True"){
 			let replies = Math.floor(Math.random()*www.length);
 			let replyMsg = www[replies];
-			await message.channel.send(replyMsg);
-			console.log("www");
-	        console.log(`from ${interaction.guild.name}`);
-            console.log(`by ${interaction.user.username}`);
-            console.log(`At ${datetime}`);
-			console.log("------");
+			let date = new Date();
+			let datetime = date.getFullYear() + "-"
+					+ (date.getMonth() + 1) + "-"
+		            + date.getDate() + " "
+					+ date.getHours() + ":"
+		            + date.getMinutes() + ":"
+		            + date.getSeconds();
+			try {
+				await message.channel.send(replyMsg);
+				console.log("www");
+	        	console.log(`from ${message.guild.name}`);
+            	console.log(`by ${message.author.tag}`);
+            	console.log(`At ${datetime}`);
+				console.log("------");
+			} catch(error){
+				const channelName = client.channels.cache.get(message.channelId);
+				
+				if(message.guild === null){
+					console.log("Type: Direct Message")
+					console.log(`by ${message.author.tag}`);
+	        		console.log(`At ${datetime}`);
+					console.log("------");
+					return;
+				} else {
+					await message.author.send(`我在${message.guild.name}的${channelName}沒有發送訊息的權限喔，請給我發送訊息的權限後再試一次`);
+				}
+				console.log(`Error at:${datetime}\n ${error}`);
+				console.log("--")
+			}
+			
 		} else {
 			return;
 		};
