39bf5303121ec3664a97c514364117bb5cf5febb
dragonyc1002 <mivck1012@gmail.com>
2022-09-01 17:05:57 +0800

    removed: interactionCreate logs

--- src/js/events/interactionCreate.cjs
+++ src/js/events/interactionCreate.cjs
@@ -1,10 +1,11 @@
-module.exports = {
+/*module.exports = {
 	name: "interactionCreate",
 	execute(interaction) {
-		if(!interaction.guild.name) return;
+		if(!interaction.isChatInputCommand()) return;
 
 		console.log(`${interaction.user.tag} 於 ${interaction.guild.name} 觸發了一次交互`);
 
 		console.log("--");
 	}
 }
+*/
