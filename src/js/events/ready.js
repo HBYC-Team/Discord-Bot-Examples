9f82930363db753cdc960b744f3bc3f0c49669c1
dragonyc1002 <mivck1012@gmail.com>
2022-09-01 16:48:16 +0800

    chore: changed rich presence on startup

--- src/js/events/ready.js
+++ src/js/events/ready.js
@@ -22,7 +22,7 @@ module.exports = {
         console.log("------------------------");
         console.log(`Now Services in ${client.guilds.cache.size} guilds`)
 		console.log("------------------------");
-		client.user.setPresence({ activities: [{ name: `在 ${client.guilds.cache.size} 個伺服器中服務\(´･ω･\`\)` }] });
+		client.user.setPresence({ activities: [{ name: `在 ${client.guilds.cache.size} 個伺服器中被壓榨\(´･ω･\`\)` }] });
 		console.log(client.guilds.cache.map(guild => guild.name));
 		console.log(client.guilds.cache.map(guild => guild.id));
 		console.log("------------------------");
