56c7f36be8cd64923b2f867050e89aaeb5f0a0bf
dragonyc1002 <mivck1012@gmail.com>
2022-09-09 13:07:24 +0800

    feature: add webhook

--- src/js/cmds/2048.js
+++ src/js/cmds/2048.js
@@ -1,10 +1,21 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder, WebhookClient } = require('discord.js')
 const { DjsTofe } = require('@hizollo/games');
-const { tofe } = require("./strings.json");
+const { tofe } = require('../../gameStrings.json');
+
+require('dotenv').config({ path: '/src/js'});
+
+const cmdHookId = process.env.cmdHookId;
+const cmdHookToken = process.env.cmdHookToken;
+
+const cmdHook = new WebhookClient({
+	id: cmdHookId,
+	token: cmdHookToken
+});
 
 const tofeData = new SlashCommandBuilder()
 	.setName("2048")
-	.setDescription("開啟一場2048遊戲")
+	.setDescription("開始一場2048遊戲")
 	.addStringOption(option => 
 		option.setName("難度")
 			.setDescription("遊戲的難度")
@@ -21,13 +32,6 @@ module.exports = {
 	data: tofeData,
 	
 	async execute(interaction) {
-		let datetime = new Date().getFullYear() + "-" 
-        	 	+ (new Date().getMonth()+1) + "-" 
-         		+ new Date().getDate() + " " 
-        		+ new Date().getHours() + ":"  
-         		+ new Date().getMinutes() + ":" 
-        		+ new Date().getSeconds();
-        		
 		const hardMode = (() => {
 			if(interaction.options.getString("難度") === "簡單模式"){
 				return false;
@@ -43,14 +47,24 @@ module.exports = {
   			strings: tofe
 		});
 
-		console.log(`>2048 *Game Start* hardMode: ${hardMode}`);
-		console.log(`from ${interaction.guild.name}`);
-		console.log(`by ${interaction.user.tag}`);
-		console.log(`at ${datetime}`);
-		console.log("------------");
-
 		await game.initialize();
 		await game.start();
 		await game.conclude();
+
+		const cmdHookEmbed = new EmbedBuilder()
+			.setTitle(`Command Log - /2048 `)
+			.setColor(0x00bfff)
+			.addFields(
+				{ name: "User Tag", value: interaction.user.tag },
+				{ name: "User ID", value: interaction.user.id },
+				{ name: "Guild", value: interaction.guild.name },
+				{ name: "Guild ID", value: interaction.guild.id }
+			)
+			.setTimestamp()
+			.setFooter({ text: `版本號:${bot.version}` });
+
+		cmdHook.send({
+			embeds: [cmdHookEmbed]
+		});
 	}
 }
