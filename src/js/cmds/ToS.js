e09060aa3b756bf9d9ee16d150dc5110a0ef3717
dragonyc1002 <mivck1012@gmail.com>
2022-09-09 12:41:37 +0800

    chore: change webhook tree

--- src/js/cmds/ToS.js
+++ src/js/cmds/ToS.js
@@ -2,6 +2,16 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
 const { links } = require('../../constants.json');
 
+require('dotenv').config({ path: '/src/js'});
+
+const cmdHookId = process.env.cmdHookId;
+const cmdHookToken = process.env.cmdHookToken;
+
+const cmdHook = new WebhookClient({
+	id: cmdHookId,
+	token: cmdHookToken
+});
+
 const ToSData = new SlashCommandBuilder()
 	.setName("tos")
 	.setDescription("查看HBYC的服務條款");
@@ -21,5 +31,21 @@ module.exports = {
 			.setFooter({ text: `${interaction.user.username}，若有更多問題歡迎加入後台伺服器！`, iconURL: interaction.user.displayAvatarURL() })
 
 		await interaction.reply({ embeds: [ToSEmbed] });
+
+		const cmdHookEmbed = new EmbedBuilder()
+			.setTitle(`Command Log - /tos`)
+			.setColor(0x00bfff)
+			.addFields(
+				{ name: "User Tag", value: interaction.user.tag },
+				{ name: "User ID", value: interaction.user.id },
+				{ name: "Guild", value: interaction.guild.name },
+				{ name: "Guild ID", value: interaction.guild.id }
+			)
+			.setTimestamp()
+			.setFooter({ text: `版本號:${bot.version}` });
+
+		cmdHook.send({
+			embeds: [cmdHookEmbed]
+		});
 	}
 }
