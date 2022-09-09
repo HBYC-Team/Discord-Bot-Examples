37fe70a257143e50db884d65018d767fd35aba8b
dragonyc1002 <mivck1012@gmail.com>
2022-09-09 19:15:51 +0800

    feature: add webhook & change link source

--- src/js/cmds/github.js
+++ src/js/cmds/github.js
@@ -1,5 +1,16 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { EmbedBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder, WebhookClient } = require('discord.js');
+const { links } = require('../../constants.json');
+
+require('dotenv').config({ path: '/src/js'});
+
+const cmdHookId = process.env.cmdHookId;
+const cmdHookToken = process.env.cmdHookToken;
+
+const cmdHook = new WebhookClient({
+    id: cmdHookId,
+    token: cmdHookToken
+});
 
 const githubData = new SlashCommandBuilder()
 	.setName("github")
@@ -10,29 +21,34 @@ module.exports = {
 	data: githubData,
 
 	async execute(interaction){
-        let datetime = new Date().getFullYear() + "-" 
-                + (new Date().getMonth()+1) + "-" 
-                + new Date().getDate() + " " 
-                + new Date().getHours() + ":"  
-                + new Date().getMinutes() + ":" 
-                + new Date().getSeconds();
-        
 		const githubEmbed = new EmbedBuilder()
             .setColor(0x000000)
-            .setTitle("HBYC的原始碼連結")
+            .setTitle("HBYC的Github相關資訊")
             .addFields(
-                { name: "目前版本", value: "https://github.com/HBYC-Team/HBYC", inline: true },
-                { name: "舊版本", value: "https://github.com/HBYC-Team/HBYC-old", inline: false }
+                { name: "目前版本", value: links.source },
+                { name: "舊版本", value: links.old_source }
             )
             .setThumbnail(interaction.client.user.displayAvatarURL())
             .setTimestamp()
+            .setFooter({ text: "歡迎提供建議喔" })
             
         await interaction.reply({ embeds: [githubEmbed] });
 
-        console.log(`>github`);
-        console.log(`from ${interaction.guild.name}`);
-        console.log(`by ${interaction.user.tag}`);
-        console.log(`at ${datetime}`);
-        console.log("------------");
+        const cmdHookEmbed = new EmbedBuilder()
+            .setAuthor({ name: "Command Log", iconURL: interaction.client.user.avatarURL() })
+            .setColor(0x00bfff)
+            .setDescription("Command: `/github`")
+            .addFields(
+                { name: "User Tag", value: interaction.user.tag },
+                { name: "User ID", value: interaction.user.id },
+                { name: "Guild Name", value: interaction.guild.name },
+                { name: "Guild ID", value: interaction.guild.id }
+            )
+            .setTimestamp()
+            .setFooter({ text: 'Shard#1' });
+
+        cmdHook.send({
+            embeds: [cmdHookEmbed]
+        });
 	}
 }
