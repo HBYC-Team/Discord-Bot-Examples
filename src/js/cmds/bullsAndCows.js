cda99cd8e2bfc398330b8de21d359fbd2e94fdb2
dragonyc1002 <mivck1012@gmail.com>
2022-09-09 13:37:41 +0800

    feature: add webhook

--- src/js/cmds/bullsAndCows.js
+++ src/js/cmds/bullsAndCows.js
@@ -1,10 +1,22 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder, WebhookClient } = require('discord.js')
 const { DjsBullsAndCows } = require('@hizollo/games');
-const { bullsAndCows } = require("./strings.json")
+const { bullsAndCows } = require('../../gameStrings.json');
+const { bot } = require('../../constants.json');
+
+require('dotenv').config({ path: '/src/js'});
+
+const cmdHookId = process.env.cmdHookId;
+const cmdHookToken = process.env.cmdHookToken;
+
+const cmdHook = new WebhookClient({
+    id: cmdHookId,
+    token: cmdHookToken
+});
 
 const bullsAndCowsData = new SlashCommandBuilder()
 	.setName("bullsandcows")
-	.setDescription("開啟一場猜AB遊戲")
+	.setDescription("遊玩一場猜AB遊戲！")
 	.addStringOption(option =>
 		option.setName("難度")
 		.setDescription("遊戲難度")
@@ -18,13 +30,6 @@ module.exports = {
 	data: bullsAndCowsData,
 
 	async execute(interaction) {
-		let datetime = new Date().getFullYear() + "-" 
-        	 	+ (new Date().getMonth()+1) + "-" 
-         		+ new Date().getDate() + " " 
-        		+ new Date().getHours() + ":"  
-         		+ new Date().getMinutes() + ":" 
-        		+ new Date().getSeconds();
-		
 		const hardMode = (() => {
 			if(interaction.options.getString("難度") === "簡單模式"){
 				return false;
@@ -40,15 +45,24 @@ module.exports = {
 			strings: bullsAndCows
 		});
 
-		console.log(`>bullsandcows *Game Start* hardMode: ${hardMode}`);
-		console.log(`from ${interaction.guild.name}`);
-		console.log(`by ${interaction.user.tag}`);
-		console.log(`at ${datetime}`);
-		console.log("------------");
-
 		await game.initialize();
 		await game.start();
 		await game.conclude();
 
-	},
+		const cmdHookEmbed = new EmbedBuilder()
+            .setTitle(`Command Log - /bullsandcows`)
+            .setColor(0x00bfff)
+            .addFields(
+                { name: "User Tag", value: interaction.user.tag },
+                { name: "User ID", value: interaction.user.id },
+                { name: "Guild", value: interaction.guild.name },
+                { name: "Guild ID", value: interaction.guild.id }
+            )
+            .setTimestamp()
+            .setFooter({ text: bot.version });
+
+        cmdHook.send({
+            embeds: [cmdHookEmbed]
+        }); 
+	}
 }
