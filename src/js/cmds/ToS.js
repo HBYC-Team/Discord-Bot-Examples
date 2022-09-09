bfff03d28822c76c38fc01a1b7bf218bc1507000
dragonyc1002 <mivck1012@gmail.com>
2022-09-09 11:38:21 +0800

    refactor: import link from constants.json

--- src/js/cmds/ToS.js
+++ src/js/cmds/ToS.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { EmbedBuilder } = require("discord.js");
+const { links } = require('../../constants.json');
 
 const ToSData = new SlashCommandBuilder()
 	.setName("tos")
@@ -9,28 +10,16 @@ module.exports = {
 	data: ToSData,
 
 	async execute(interaction){
-		let datetime = new Date().getFullYear() + "-"
-					+ (new Date().getMonth()+1) + "-"
- 					+ new Date().getDate() + " "
-					+ new Date().getHours() + ":"
-                    + new Date().getMinutes() + ":"
-					+ new Date().getSeconds();
 
 		const ToSEmbed = new EmbedBuilder()
 			.setTitle("HBYC的服務條款")
 			.addFields(
-				{ name: "中文版", value: "https://github.com/HBYC-Team/HBYC/tree/master/docs/TeamofService-Tw.md" },
-				{ name: "English Version", value: "https://github.com/HBYC-Team/HBYC/tree/master/docs/TeamofService.md" }
+				{ name: "中文版", value: links.tos_en },
+				{ name: "English Version", value: links.tos_zh }
 			)
 			.setTimestamp()
-			.setFooter({ text: `${interaction.user.username}，若有更多問題歡迎與作者聯絡！`, iconURL: interaction.user.displayAvatarURL() })
+			.setFooter({ text: `${interaction.user.username}，若有更多問題歡迎加入後台伺服器！`, iconURL: interaction.user.displayAvatarURL() })
 
 		await interaction.reply({ embeds: [ToSEmbed] });
-		
-		console.log(">tos");
-        console.log(`from ${interaction.guild.name}`);
-        console.log(`by ${interaction.user.tag}`);
-		console.log(`at ${datetime}`);
-        console.log("------------");
 	}
 }
