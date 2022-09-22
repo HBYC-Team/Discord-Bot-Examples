f129c10dba00d1a193df72550c15b5e95d26253b
dragonyc1002 <mivck1012@gmail.com>
2022-09-22 18:42:29 +0800

    chore: change content to v2.1.0

--- src/js/cmds/announcement.js
+++ src/js/cmds/announcement.js
@@ -1,7 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { EmbedBuilder, WebhookClient } = require('discord.js');
 
-require('dotenv').config({ path: '/src/js'});
+const { bot } = require('../../constants.json');
+
+require('dotenv').config({ path: '/src/js' });
 
 const cmdHookId = process.env.cmdHookId;
 const cmdHookToken = process.env.cmdHookToken;
@@ -24,8 +26,8 @@ module.exports = {
 			.setColor(0x1cb7ca)
 			.setTitle(`最新版本公告 - ${bot.version}`)
 			.addFields(
-				{ name: ">> 使用者方面", value: "+ 將已知的bug修正\n+ 將`/report`指令的回報內容整理後直接送至後台伺服器\n+ 新增一些神奇的回應\n+ 將`/presence`指令新增一些活動狀態" },
-				{ name: ">> 專案庫方面", value: "+ Rewrite 大部分的指令\n+ 將buttonRole, join, osu的副檔名改成.cjs\n+ 新增webhook的一些使用\n+ 修改一些資料的引入方式" }
+				{ name: ">> 使用者方面", value: "+ 當HBYC被@everyone或是被@身份組時，不會再亂吵了\n+ 當HBYC同時遇到兩個不同的隱藏回應時，只會回應其中一個\n+ 當/tictactoe指令的參數錯誤時，機器人不會私訊你了" },
+				{ name: ">> 專案庫方面", value: "+ 將漏掉的webhook添加\n+ 將錯誤分支號碼更改為正確號碼+ 將catch(error)的部份簡化\n+ 將avatar.ts的部份內容稍做更改\n- 把沒用到的import移除\n+ 將已知的bug修正" }
 			)
 			.setTimestamp()
 			.setFooter({ text: "更詳細的內容請到Github上查看喔！" });
