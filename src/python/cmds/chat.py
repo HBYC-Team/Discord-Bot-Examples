db7592ff5d62c5bd44a62b601ad838875f66e2b7
sus2790 <104747821+sus2790@users.noreply.github.com>
2022-10-07 18:12:22 +0800

    Update chat.py

--- src/python/cmds/chat.py
+++ src/python/cmds/chat.py
@@ -1,98 +1,101 @@
-import discord
-
-from discord.commands import slash_command, Option
+from discord import Option
+from discord.ext import commands
+from discord.ext.commands import slash_command
 from core.classes import Cog_Extension
 
-import json, os
+import os
+import time
+import json
 
-with open("pyconfig.json", mode="r", encoding="utf8") as pyconfig:
+with open("cmds/pyconfig.json", encoding="utf-8") as pyconfig:
 	pyconf = json.load(pyconfig)
 
 class Chat(Cog_Extension):
 	@slash_command(name="say", descirption="讓機器人說出一句話")
-	async def say(self, ctx,* , content: Option(str, "訊息內容", required=true)):
-		await ctx.respond("你的訊息已經成功傳送", ephemeral=true)
+	async def say(self, ctx, *, content: Option(str, "訊息內容", required=True)):
+		await ctx.respond("你的訊息已經成功傳送", ephemeral=True)
 		await ctx.send(content)
 
 	@slash_command(name="echo", descirption="讓機器人重複你說的一句話")
-	async def echo(self, ctx, *, content: Option(str, "訊息內容", required=true)):
+	async def echo(self, ctx, *, content: Option(str, "訊息內容", required=True)):
 		await ctx.respond(content)
 
 	@slash_command(name="thinking", description="w")
 	async def thinking(self, ctx, 種類: Option(str, choices=pyconf["thinking"], required=True)):
 		print("/thinking", 種類)
-		print("from", ctx.author.guild.name)
-		print("at", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+		print("from:", ctx.author.guild.name)
+		print("at:", time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
 		print("by:", ctx.author)
 		print("------")
-        
+				
 		if 種類 == None:
-            await ctx.respond("<:thinking:974621588257398784>已傳送", ephemeral=True)
-            await ctx.send("<:thinking:974621588257398784>")
-
-		if 種類 == "normal":
-		    await ctx.respond("<:thinking:974621588257398784>已傳送", ephemeral=True)
-		    await ctx.send("<:thinking:974621588257398784>")
+			await ctx.respond("<:thinking:974621588257398784>已傳送", ephemeral=True)
+			await ctx.send("<:thinking:974621588257398784>")
+	  
+		if 種類 == "normal":    
+			await ctx.respond("<:thinking:974621588257398784>已傳送", ephemeral=True)
+			await ctx.send("<:thinking:974621588257398784>")
 
 		if 種類 == "cat":
-            await ctx.respond("<:cathink:985794732926074900>已傳送", ephemeral=True)
-            await ctx.send("<:cathink:985794732926074900>")
+			await ctx.respond("<:cathink:985794732926074900>已傳送", ephemeral=True)
+			await ctx.send("<:cathink:985794732926074900>")
 
 		if 種類 == "attano":
-            await ctx.respond("<:attanothink:984310669425930251>已傳送", ephemeral=True)
-            await ctx.send("<:attanothink:984310669425930251>")
-        
+			await ctx.respond("<:attanothink:984310669425930251>已傳送", ephemeral=True)
+			await ctx.send("<:attanothink:984310669425930251>")
+				
 		if 種類 == "thonk":
-            await ctx.respond("<:thonk:984310370363645962>已傳送", ephemeral=True)
-            await ctx.send("<:thonk:984310370363645962>")
-        
+			await ctx.respond("<:thonk:984310370363645962>已傳送", ephemeral=True)
+			await ctx.send("<:thonk:984310370363645962>")
+				
 		if 種類 == "superthonk":
-            await ctx.respond("<:superthonk:984310368790781992>已傳送", ephemeral=True)
-            await ctx.send("<:superthonk:984310368790781992>")
+			await ctx.respond("<:superthonk:984310368790781992>已傳送", ephemeral=True)
+			await ctx.send("<:superthonk:984310368790781992>")
 
 		if 種類 == "raythonk":
-            await ctx.respond("<:raythonk:984310421072773170>已傳送", ephemeral=True)
-            await ctx.send("<:raythonk:984310421072773170>")
+			await ctx.respond("<:raythonk:984310421072773170>已傳送", ephemeral=True)
+			await ctx.send("<:raythonk:984310421072773170>")
 
 		if 種類 == "rainbowhtonk":
-            await ctx.respond("<:rainbowthonk:984310367276650546>已傳送", ephemeral=True)
-            await ctx.send("<:rainbowthonk:984310367276650546>")
+			await ctx.respond("<:rainbowthonk:984310367276650546>已傳送", ephemeral=True)
+			await ctx.send("<:rainbowthonk:984310367276650546>")
 
 		if 種類 == "owothonk":
-    		await ctx.respond("<:owothonk:984310416672960553>已傳送", ephemeral=True)
+			await ctx.respond("<:owothonk:984310416672960553>已傳送", ephemeral=True)
 			await ctx.send("<:owothonk:984310416672960553>")
 
 		if 種類 == "thongk":
-            await ctx.respond("<:thongk:984310425648779325>已傳送", ephemeral=True)
-            await ctx.send("<:thongk:984310425648779325>")
+			await ctx.respond("<:thongk:984310425648779325>已傳送", ephemeral=True)
+			await ctx.send("<:thongk:984310425648779325>")
 
 		if 種類 == "smile1":
-            await ctx.respond("<:smilethonk:984310424155611206>已傳送", ephemeral=True)
-            await ctx.send("<:smilethonk:984310424155611206>")
+			await ctx.respond("<:smilethonk:984310424155611206>已傳送", ephemeral=True)
+			await ctx.send("<:smilethonk:984310424155611206>")
 
 		if 種類 == "smile2":
-            await ctx.respond("<:w_:984310372091703296>已傳送", ephemeral=True)
-            await ctx.send("<:w_:984310372091703296>")
+			await ctx.respond("<:w_:984310372091703296>已傳送", ephemeral=True)
+			await ctx.send("<:w_:984310372091703296>")
 
 		if 種類 == "rayteethonk":
-            await ctx.respond("<:rayteethonk:984310422637281342>已傳送", ephemeral=True)
-            await ctx.send("<:rayteethonk:984310422637281342>")
+			await ctx.respond("<:rayteethonk:984310422637281342>已傳送", ephemeral=True)
+			await ctx.send("<:rayteethonk:984310422637281342>")
 
 		if 種類 == "blue":
-            await ctx.respond("<:bluethonk:984310412256358450>已傳送", ephemeral=True)
-            await ctx.send("<:bluethonk:984310412256358450>")
+			await ctx.respond("<:bluethonk:984310412256358450>已傳送", ephemeral=True)
+			await ctx.send("<:bluethonk:984310412256358450>")
 
 		if 種類 == "10":
-            await ctx.respond("<:10thonk:984310410738028604>已傳送", ephemeral=True)
-            await ctx.send("<:10thonk:984310410738028604>")
+			await ctx.respond("<:10thonk:984310410738028604>已傳送", ephemeral=True)
+			await ctx.send("<:10thonk:984310410738028604>")
 
 		if 種類 == "distortion":
-            await ctx.respond("<:distrotionthonk:984310414097657877>已傳送", ephemeral=True)
-            await ctx.send("<:distrotionthonk:984310414097657877>")
+			await ctx.respond("<:distrotionthonk:984310414097657877>已傳送", ephemeral=True)
+			await ctx.send("<:distrotionthonk:984310414097657877>")
 
 		if 種類 == "pistol":
-            await ctx.respond("<:pisthonk:984310418455539742>", ephemeral=True)
-            await ctx.send("<:pisthonk:984310418455539742>")
+			await ctx.respond("<:pisthonk:984310418455539742>", ephemeral=True)
+			await ctx.send("<:pisthonk:984310418455539742>")
+
 
 def setup(client):
-	client.add_cog(client(Chat))
+	client.add_cog(Chat(client))
